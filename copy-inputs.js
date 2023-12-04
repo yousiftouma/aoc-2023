7e823fedb2b39b7e0692334fb447134df3af73e3
Yousif Touma <yousif.touma@stenaline.com>
2023-12-04 17:20:10 +0100

    try new copy inputs that works on both mac and windows

--- copy-inputs.js
+++ copy-inputs.js
@@ -1,16 +1,28 @@
-const rsync = require('rsync');
+// const rsync = require("rsync");
+//
+// rsync()
+//   .flags("a") // Archive mode (recursive copy + retain attributes)
+//   .source("src/")
+//   .destination("dist/")
+//   .exclude("**/*.ts") // Exclude TypeScript files
+//   .exclude("**/*.map") // Exclude source maps
+//   .execute((error, code, cmd) => {
+//     if (error) {
+//       console.log(code, cmd);
+//       console.error(`Error copying input files: ${error}`);
+//       process.exit(1);
+//     } else {
+//       console.log("Input files copied successfully.");
+//     }
+//   });
 
-rsync()
-    .flags('a') // Archive mode (recursive copy + retain attributes)
-    .source('src/')
-    .destination('dist/')
-    .exclude('**/*.ts') // Exclude TypeScript files
-    .exclude('**/*.map') // Exclude source maps
-    .execute((error, code, cmd) => {
-        if (error) {
-            console.error(`Error copying input files: ${error}`);
-            process.exit(1);
-        } else {
-            console.log('Input files copied successfully.');
-        }
-    });
+const copyfiles = require("copyfiles");
+
+copyfiles(["src/**/*.txt", "dist"], { all: true, up: 1 }, (error) => {
+  if (error) {
+    console.error(`Error copying input files: ${error}`);
+    process.exit(1);
+  } else {
+    console.log("Input files copied successfully.");
+  }
+});
