89055e8e77c25d7c459e0d317cfd2d25d0aa0ef5
Yousif Touma <yousif.touma@hollytech.se>
2023-12-04 20:47:26 +0100

    WIP day2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,8 +1,30 @@
 export const solve = (input: string): string => {
-    // Your solution logic for day 1 goes here
-    // Parse the input, perform calculations, and return the result
-    const numbers = input.split('\n').map(Number);
-    const sum = numbers.reduce((acc, num) => acc + num, 0);
-  
-    return sum.toString();
-  };
+  // Your solution logic for day 1 goes here
+  // Parse the input, perform calculations, and return the result
+  const lines = input.split("\n");
+  const result = solveFirst(lines);
+
+  return result;
+};
+
+const solveFirst = (input: Array<string>): string => {
+  // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  // return ID when possible, 0 when impossible
+  const sum = input.map((line) => {
+    const maxRed = 12;
+    const maxGreen = 13;
+    const maxBlue = 14;
+    // get the sets
+    const sets = line.split(":")[1].split(";");
+    // a set 1 blue, 2 green
+    for (let i = 0; i < sets.length; i++) {}
+
+    return getGameId(line);
+  });
+
+  return sum.toString();
+};
+
+const getGameId = (line: string): number => {
+  return Number.parseInt(line.split(":")[0].split(" ")[1]);
+};
