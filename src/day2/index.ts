3c7b7ca63c99ae0feff65b75554ac56286fe3ba4
Yousif Touma <yousif.touma@hollytech.se>
2023-12-04 20:56:38 +0100

    day2 part 1 done

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,3 +1,5 @@
+import { sum } from "lodash";
+
 export const solve = (input: string): string => {
   // Your solution logic for day 1 goes here
   // Parse the input, perform calculations, and return the result
@@ -10,19 +12,38 @@ export const solve = (input: string): string => {
 const solveFirst = (input: Array<string>): string => {
   // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
   // return ID when possible, 0 when impossible
-  const sum = input.map((line) => {
+  const values = input.map((line) => {
     const maxRed = 12;
     const maxGreen = 13;
     const maxBlue = 14;
-    // get the sets
+    // get the sets, e.g. 1 blue, 2 green; 3 green, 4 blue, 1 red
     const sets = line.split(":")[1].split(";");
-    // a set 1 blue, 2 green
-    for (let i = 0; i < sets.length; i++) {}
+    for (let i = 0; i < sets.length; i++) {
+      // a set, e.g. 1 blue, 2 green
+      const set = sets[i];
+      const colors = set.split(",");
+      for (let j = 0; j < colors.length; j++) {
+        // a color, e.g. " 1 blue"
+        const currentColor = colors[j];
+        const parts = currentColor.split(" ");
+        const name = parts[2];
+        const num = Number.parseInt(parts[1]);
+        if (name === "red" && num > maxRed) {
+          return 0;
+        }
+        if (name === "blue" && num > maxBlue) {
+          return 0;
+        }
+        if (name === "green" && num > maxGreen) {
+          return 0;
+        }
+      }
+    }
 
     return getGameId(line);
   });
 
-  return sum.toString();
+  return sum(values).toString();
 };
 
 const getGameId = (line: string): number => {
