02eb931ce1eb6773db966cc9e67376a6e8acdfab
Yousif Touma <yousif.touma@stenaline.com>
2023-12-05 08:57:49 +0100

    finish day2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -4,9 +4,46 @@ export const solve = (input: string): string => {
   // Your solution logic for day 1 goes here
   // Parse the input, perform calculations, and return the result
   const lines = input.split("\n");
-  const result = solveFirst(lines);
+  //const result = solveFirst(lines);
+  const result = solveSecond(lines);
 
   return result;
+};
+
+const solveSecond = (input: Array<string>): string => {
+  // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+  // get max for each color and multiply
+  const values = input.map((line) => {
+    let maxRed = 0;
+    let maxGreen = 0;
+    let maxBlue = 0;
+    // get the sets, e.g. 1 blue, 2 green; 3 green, 4 blue, 1 red
+    const sets = line.split(":")[1].split(";");
+    for (let i = 0; i < sets.length; i++) {
+      // a set, e.g. 1 blue, 2 green
+      const set = sets[i];
+      const colors = set.split(",");
+      for (let j = 0; j < colors.length; j++) {
+        // a color, e.g. " 1 blue"
+        const currentColor = colors[j];
+        const parts = currentColor.split(" ");
+        const name = parts[2];
+        const num = Number.parseInt(parts[1]);
+        if (name.includes("red") && num > maxRed) {
+          maxRed = num;
+        }
+        if (name.includes("blue") && num > maxBlue) {
+          maxBlue = num;
+        }
+        if (name.includes("green") && num > maxGreen) {
+          maxGreen = num;
+        }
+      }
+    }
+    return maxRed * maxGreen * maxBlue;
+  });
+
+  return sum(values).toString();
 };
 
 const solveFirst = (input: Array<string>): string => {
